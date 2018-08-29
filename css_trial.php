6a397fcaef6ad5340450c66753cc289dc6fdcebf
Keval Patel <keval22np@gmail.com>
2018-08-29 21:23:36 +0530

    new

---
+++ css_trial.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link href="clean.css" rel="stylesheet" type="text/css">
+<title>Welcome to InfoPanel</title>
+</head>
+
+<body>
+	<div></div>
+</body>
+</html>
